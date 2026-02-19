b7a783b7ba0de019696c5eebe4fca7ad619920dc
Yang Zhang <rickzh@umich.edu>
2026-02-19 13:42:13 -0500

    aero_logic

--- aero_logic.py
+++ aero_logic.py
@@ -3,7 +3,7 @@ Module: Aero Lookup Table Logic
 Author: Zhang
 Original Ref: OptimalFinAngle.cpp
 Description: 
-    Aerodynamic decision-making module. Uses the look-up talbel
+    Aerodynamic decision-making module. Uses the look-up tabel
     to determine the optimal fin attack angle (Target Angle) 
     based on the real-time wind speed received from the CAN bus.
 """
